046f17de5def3a73cdba6aca4640ef3d44a32611
pedroping <84201034+pedroping@users.noreply.github.com>
2023-04-19 18:49:45 -0300

    1 file modified

--- src/app/ad-banner.component.ts
+++ src/app/ad-banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, ComponentFactoryResolver, Input, OnDestroy, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 
 import { AdDirective } from './ad.directive';
 import { AdItem } from './ad-item';
@@ -10,6 +10,7 @@ import { AdComponent } from './ad.component';
     <div class="ad-banner-example">
       <h3>Advertisements</h3>
       <ng-template adHost></ng-template>
+      <ng-template #Teste></ng-template>
     </div>
   `
 })
@@ -19,9 +20,13 @@ export class AdBannerComponent implements OnInit, OnDestroy {
   currentAdIndex = -1;
 
   @ViewChild(AdDirective, {static: true}) adHost!: AdDirective;
+  @ViewChild('Teste', { read: ViewContainerRef }) Teste!: ViewContainerRef; 
 
   private clearTimer: VoidFunction | undefined;
 
+  constructor(private r: ComponentFactoryResolver){
+
+  }
   ngOnInit(): void {
     this.loadComponent();
     this.getAds();
@@ -33,14 +38,18 @@ export class AdBannerComponent implements OnInit, OnDestroy {
 
   loadComponent() {
     this.currentAdIndex = (this.currentAdIndex + 1) % this.ads.length;
-    console.log("this.currentAdIndex", this.currentAdIndex)
     const adItem = this.ads[this.currentAdIndex];
 
     const viewContainerRef = this.adHost.viewContainerRef;
+    const viewContainerRef2 = this.Teste?.clear()
     viewContainerRef.clear();
 
     const componentRef = viewContainerRef.createComponent(adItem.component);
     componentRef.instance.data = adItem.data;
+    
+    const Teste = this.Teste?.createComponent(adItem.component)
+    if(Teste)
+      Teste.instance.data = adItem.data;
   }
 
   getAds() {
