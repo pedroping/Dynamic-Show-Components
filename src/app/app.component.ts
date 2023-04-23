e3c6970136a93545cc7a67a103cb1142654dab3c
pedroping <84201034+pedroping@users.noreply.github.com>
2023-04-23 19:42:24 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { HeroJobAdComponent } from './hero-job-ad.component';
     <div>
       <app-ad-banner [ads]="ads"></app-ad-banner>
     </div>
-  `
+  `,
 })
 export class AppComponent implements OnInit {
   ads: AdItem[] = [];
@@ -21,27 +21,49 @@ export class AppComponent implements OnInit {
   }
   getAds() {
     return [
-      new AdItem(
-        HeroProfileComponent,
-        { name: 'Bombasto', bio: 'Brave as they come' }
-      ),
-      new AdItem(
-        HeroProfileComponent,
-        { name: 'Dr. IQ', bio: 'Smart as they come' }
-      ),
-      new AdItem(
-        HeroJobAdComponent,
-        { headline: 'Hiring for several positions', body: 'Submit your resume today!' }
-      ),
-      new AdItem(
-        HeroJobAdComponent,
-        { headline: 'Openings in all departments', body: 'Apply today' }
-      )
+      new AdItem(HeroProfileComponent, {
+        name: 'Bombasto',
+        bio: 'Brave as they come',
+      }),
+      new AdItem(HeroProfileComponent, {
+        name: 'Dr. IQ',
+        bio: 'Smart as they come',
+      }),
+      new AdItem(HeroJobAdComponent, {
+        headline: 'Hiring for several positions',
+        body: 'Submit your resume today!',
+      }),
+      new AdItem(HeroJobAdComponent, {
+        headline: 'Openings in all departments',
+        body: 'Apply today',
+      }),
+    ];
+  }
+
+  newAds() {
+    return [
+      {
+        component: HeroProfileComponent,
+        data: { name: 'Bombasto', bio: 'Brave as they come' },
+      },
+      {
+        component: HeroProfileComponent,
+        data: { name: 'Dr. IQ', bio: 'Smart as they come' },
+      },
+      {
+        component: HeroProfileComponent,
+        data: {
+          headline: 'Hiring for several positions',
+          body: 'Submit your resume today!',
+        },
+      },
+      {
+        component: HeroProfileComponent,
+        data: { headline: 'Openings in all departments', body: 'Apply today' },
+      },
     ];
   }
 }
-
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
